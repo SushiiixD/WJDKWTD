69a3350db018d46eff4275ae4b8636f238296941
Bog <berenger.ossete@live.fr>
2015-01-23 23:51:22 +0100

    we can move entities now

--- core/src/com/gameconcoillote/ijdkwtd/ijdkwtd.java
+++ core/src/com/gameconcoillote/ijdkwtd/ijdkwtd.java
@@ -4,19 +4,35 @@ package com.gameconcoillote.ijdkwtd;
 import java.util.ArrayList;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
 
-public class ijdkwtd extends Game{
+public class ijdkwtd extends Game
+{
+	
 	Screen inGameScreen;
+	//contain all the displayable an updatable objects
 	private ArrayList<Entity> entities = new  ArrayList<Entity>();
+	
+	
     @Override
-    public void create () {
+    public void create()
+    {
         inGameScreen = new InGameScreen();
         this.setScreen(inGameScreen);
+        
+        // for testing
+        /*Texture img = new Texture(Gdx.files.internal("badlogic.jpg"));
+        Entity abc = new Entity(img);
+        abc.move(10,5);
+        entities.add(abc);*/
     }
 
     @Override
-    public void render () {       
+    public void render()
+    {       
         super.render();
         
         for(Entity e: entities)
