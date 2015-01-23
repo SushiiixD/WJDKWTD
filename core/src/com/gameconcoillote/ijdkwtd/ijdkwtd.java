acb6395b1a392ce3f2f58491c2e9126fe850375e
nomce <naum.spaseski@gmail.com>
2015-01-24 00:22:58 +0100

    Resolved diffs and added mouse click

--- core/src/com/gameconcoillote/ijdkwtd/ijdkwtd.java
+++ core/src/com/gameconcoillote/ijdkwtd/ijdkwtd.java
@@ -1,25 +1,40 @@
 package com.gameconcoillote.ijdkwtd;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import java.util.ArrayList;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
-public class ijdkwtd extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		//batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
-		setScreen(new Menu(this)); // Appel de la méthode setScreen en créant un nouvel objet Menu
-	}
-	
-	/*@Override
+public class ijdkwtd extends Game{
+	Screen inGameScreen;
+	private ArrayList<Entity> entities = new  ArrayList<Entity>();
+    @Override
+    public void create () {
+        inGameScreen = new InGameScreen();
+        this.setScreen(inGameScreen);
+    }
+
+    @Override
+    public void render () {       
+        super.render();
+        
+        for(Entity e: entities)
+        {
+        	e.update();
+        	e.draw();
+        }
+
+        if(Gdx.input.isTouched()) {
+            try {
+                Thread.sleep(200);
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Mouse: x=" + Gdx.input.getX() + " y=" + Gdx.input.getY());
+        }
+        
+        	/*@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -27,5 +42,5 @@ public class ijdkwtd extends Game {
 		batch.draw(img, 0, 0);
 		batch.end();
 	}*/
-
+    }
 }
