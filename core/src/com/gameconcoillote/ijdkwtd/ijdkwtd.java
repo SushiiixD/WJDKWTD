e1a2711954254e8872fadf8dab09750006bd35e2
Bog <berenger.ossete@live.fr>
2015-01-23 22:17:54 +0100

    petit nettoyage (ecran blanc+plus d'image+titre+dimensions

--- core/src/com/gameconcoillote/ijdkwtd/ijdkwtd.java
+++ core/src/com/gameconcoillote/ijdkwtd/ijdkwtd.java
@@ -16,12 +16,14 @@ public class ijdkwtd extends ApplicationAdapter {
 		img = new Texture("badlogic.jpg");
 	}
 
+	
+	
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
+		/*batch.begin();
 		batch.draw(img, 0, 0);
-		batch.end();
+		batch.end();*/
 	}
 }
--- desktop/src/com/gameconcoillote/ijdkwtd/desktop/DesktopLauncher.java
+++ desktop/src/com/gameconcoillote/ijdkwtd/desktop/DesktopLauncher.java
@@ -1,12 +1,27 @@
 package com.gameconcoillote.ijdkwtd.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
 import com.gameconcoillote.ijdkwtd.ijdkwtd;
 
+
 public class DesktopLauncher {
+	
+	public final static int WIDTH = 1024;
+	public final static int HEIGHT = 768;
+
+	
+	
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new ijdkwtd(), config);
+		
+		config.title="GAME JAM: IJDKWIAD";
+		config.width=WIDTH;
+		config.height=HEIGHT;
+		
+		
 	}
 }
