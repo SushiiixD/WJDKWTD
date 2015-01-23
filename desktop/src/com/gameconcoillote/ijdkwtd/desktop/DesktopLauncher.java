aefcac7fc19f0b07b57abf4b402025eb72bf6ef7
nomce <naum.spaseski@gmail.com>
2015-01-23 20:06:57 +0100

    Initial commit

---
+++ desktop/src/com/gameconcoillote/ijdkwtd/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gameconcoillote.ijdkwtd.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gameconcoillote.ijdkwtd.ijdkwtd;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ijdkwtd(), config);
+	}
+}
