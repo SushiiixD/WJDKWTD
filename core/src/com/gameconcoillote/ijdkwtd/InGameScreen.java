3984ffa05b15560da149eebf015794e389a1899f
Lloydangifer <vmanrique@netcourrier.com>
2015-01-24 17:02:18 +0100

    nettoyage de code

--- core/src/com/gameconcoillote/ijdkwtd/InGameScreen.java
+++ core/src/com/gameconcoillote/ijdkwtd/InGameScreen.java
@@ -1,87 +1,60 @@
 package com.gameconcoillote.ijdkwtd;
 
 import java.util.ArrayList;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class InGameScreen implements Screen{
- 
-    
+public class InGameScreen implements Screen{    
     private ArrayList<Entity> entities = new  ArrayList<Entity>();
 	private ArrayList<Background> level = new  ArrayList<Background>();
 	private Player player;
 	private SpriteBatch batch = new SpriteBatch();
 	
-	    public InGameScreen()
-	    {
+	public InGameScreen(){
 	        //player
 	        this.player = new Player();
 	        entities.add(this.player);
-	        
 	        //background
 	        level.add(new Background(new Texture(Gdx.files.internal("Background1.jpg"))));
-
-	    }
-	 
+	}	 
     @Override
-    public void render(float delta)
-    {
-         
-    	for(Background bg: this.level)
-        {
+    public void render(float delta){       
+    	for(Background bg: this.level){
         	bg.update((int)delta);
         	bg.draw(this.batch);
-        }
-        
-        for(Entity e: entities)
-        {
+        } 
+        for(Entity e: entities){
         	e.update((int)delta);
         	e.draw(this.batch);
         }
-
-    }
-     
-    public Player getPlayer()
-    {
+    }   
+    public Player getPlayer(){
     	return this.player;
-    }
-    
+    } 
     @Override
-    public void resize(int width, int height) {
-        // TODO Auto-generated method stub
-         
+    public void resize(int width, int height){
+        // TODO Auto-generated method stub    
     }
- 
     @Override
-    public void show() {
+    public void show(){
 		// TODO Auto-generated method stub
     }
-
 	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
+	public void pause(){
+		// TODO Auto-generated method stub	
 	}
-
 	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
+	public void resume(){
+		// TODO Auto-generated method stub		
 	}
-
 	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
+	public void hide(){
+		// TODO Auto-generated method stub	
 	}
-
 	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
+	public void dispose(){
+		// TODO Auto-generated method stub		
 	}
 }
-
