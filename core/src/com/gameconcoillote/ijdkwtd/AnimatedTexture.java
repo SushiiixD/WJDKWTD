fcb295a2f532b952ea9a8fa186ee5d9830e8b4d2
Lloydangifer <vmanrique@netcourrier.com>
2015-01-24 17:14:14 +0100

    nettoyage de code

--- core/src/com/gameconcoillote/ijdkwtd/AnimatedTexture.java
+++ core/src/com/gameconcoillote/ijdkwtd/AnimatedTexture.java
@@ -1,71 +1,46 @@
 package com.gameconcoillote.ijdkwtd;
 
 import java.util.ArrayList;
-
-import org.omg.CORBA.Current;
-
 import com.badlogic.gdx.graphics.Texture;
 
-public class AnimatedTexture implements Runnable
-{
+public class AnimatedTexture implements Runnable{
 	private ArrayList<Texture> textures = new ArrayList<Texture>(); //all the frame
 	private int t;//index of the current frame (t as Time)
 	private Thread thread = new Thread(this);
 	private int delay;//delay between two frame
 	
-	public AnimatedTexture(Texture t,int delay)
-	{
+	public AnimatedTexture(Texture t,int delay){
 		this.textures.add(t);
 		this.t=0;
 		this.delay = delay;
 		this.thread.start();
 	}
-	
-
-	
-	public AnimatedTexture(Texture t)
-	{
+	public AnimatedTexture(Texture t){
 		this(t,100);
 	}
-	
-	public void setDelay(int d)
-	{
+	public void setDelay(int d){
 		this.delay = d;
 	}
-	
-	public void update()
-	{
-		
-		try
-		{
+	public void update(){	
+		try{
 			this.thread.sleep(this.delay);
 			this.t++;
 			if(this.t >= textures.size()) this.t=0;
 		} 
-		catch (InterruptedException e) 
-		{
+		catch (InterruptedException e){
 			e.printStackTrace();
 		}
-		
-	
-	}
-	
-	public Texture getCurrentTexture()
-	{
+	}	
+	public Texture getCurrentTexture(){
 		return this.textures.get(t);
 	}
-	
-	public void addTexture(Texture tex)
-	{
+	public void addTexture(Texture tex){
 		this.textures.add(tex);
 	}
-
 	@Override
-	public void run() {
-		while(true)
-		{
+	public void run(){
+		while(true){
 			update();
-		}
-		
+		}	
 	}
 }
--- core/src/com/gameconcoillote/ijdkwtd/Entity.java
+++ core/src/com/gameconcoillote/ijdkwtd/Entity.java
@@ -1,100 +1,63 @@
 package com.gameconcoillote.ijdkwtd;
 
 import java.util.ArrayList;
-
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
-public class Entity
-{
-
-	//the box around the entity
-	protected Rectangle box;
-	//the picture of the entity
-	protected ArrayList<AnimatedTexture> textures = new ArrayList<AnimatedTexture>();
-	//the current animation
-	protected int currentAnim;
-	//max movement speed
-	protected Vector2 speed;
-	//current movement speed
-	protected Vector2 move;
+public class Entity{	
+	protected Rectangle box;//the box around the entity	
+	protected ArrayList<AnimatedTexture> textures = new ArrayList<AnimatedTexture>();//the picture of the entity
+	protected int currentAnim;//the current animation	
+	protected Vector2 speed;//max movement speed	
+	protected Vector2 move;//current movement speed
 	
-	public Entity(Texture t)
-	{
-		this(t,0,0);
-		
+	public Entity(Texture t){
+		this(t,0,0);	
 	}
-	
-	public Entity(Texture t,int x,int y)
-	{
+	public Entity(Texture t,int x,int y){
 		this.textures .add(new AnimatedTexture(t));
 		this.currentAnim = 0;
-		
 		this.box = new Rectangle(x,y,0,0);
 		this.speed = new Vector2(100,100);//default speed of 5px/s
-		this.move = new Vector2(0,0);
-		
-	}
-	
+		this.move = new Vector2(0,0);	
+	}	
 	//draw the entity on the screen
-	public void draw(SpriteBatch sprite)
-	{
+	public void draw(SpriteBatch sprite){
 		sprite.begin();
 		sprite.draw(this.textures.get(this.currentAnim).getCurrentTexture(), box.x, box.y);
 		sprite.end();
 	}
-	
 	//move the entity of Xpx et Ypx per seconds
-	public void move(int x, int y)
-	{
+	public void move(int x, int y){
 		this.move.x = x;
 		this.move.y = y;
 	}
-
-	public void update(int dt)
-	{
+	public void update(int dt){
 		//compute the speed  considering the gameloop
-		if(dt < 1000)//avoid dt error (if too big it will cause bugs)
-		{
+		if(dt < 1000){ //avoid dt error (if too big it will cause bugs)
 			this.box.x += (this.move.x * dt)/1000;
 			this.box.y += (this.move.y * dt)/1000;
-		}
-		
+		}	
 	}
-	
-	public void changeAnimation(int animIndex)
-	{
-		if(animIndex < this.textures.size())
-		{
+	public void changeAnimation(int animIndex){
+		if(animIndex < this.textures.size()){
 			this.currentAnim = animIndex;
-		}
-		
-	}
-	
-	public AnimatedTexture getAnim(int animIndex)
-	{
+		}	
+	}	
+	public AnimatedTexture getAnim(int animIndex){
 		return this.textures.get(animIndex);
 	}
-	
-	public void addTextureInAnim(Texture t,int animIndex)
-	{
-		if(animIndex < this.textures.size())
-		{
+	public void addTextureInAnim(Texture t,int animIndex){
+		if(animIndex < this.textures.size()){
 			this.textures.get(animIndex).addTexture(t);
 		}
-		else//in case we try to access to a none definited animation
-		{
+		else{ //in case we try to access to a none definited animation
 			this.textures.add(new AnimatedTexture(t));
 		}
-	}
-	
-	
-	public Vector2 getPosition()
-	{
+	}		
+	public Vector2 getPosition(){
 		return new Vector2(box.x,box.y);
 	}
-	
-	
 }
